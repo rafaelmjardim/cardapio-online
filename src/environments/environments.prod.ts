08a976c8a9fe35d0f4c8531682af827fcb303662
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-07-05 20:33:49 -0300

    update

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+
+    API_KEY: 'http://177.137.118.192:9090'
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,6 @@
+export const environment = {
+
+    production: true,
+
+    API_KEY: 'http://177.137.118.192:9090'
+};
