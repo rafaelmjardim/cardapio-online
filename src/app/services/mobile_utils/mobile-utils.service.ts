fab3be6c2809f0c8836cf07c7773f001c903e3a1
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-07-12 18:27:39 -0300

    clean code

---
+++ src/app/services/mobile_utils/mobile-utils.service.ts
@@ -0,0 +1,27 @@
+// PARA UTILIZAR ESSE SERVIÇO É PRECISO CHAMAR A FUNÇÃO ONINITBREAKPOINT NO ngAfterViewInit DO COMPONENTE
+// CASO PRECISE UTILIZAR ESTE SERVIÇO NO APP.COMPONENT SERÁ NECESSARIO UTILIZAR A FUNÇÃO this.changeDetectorRef.detectChanges(); no afterInit do app-module PARA EVITAR UM ERRO
+
+import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from "@angular/cdk/layout";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MobileUtilsService {
+  
+  breakpoints = Breakpoints; //Contem os valores de resolução(Não está em uso mas pode ser usado)
+
+  mediaMobile!: boolean;
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  onInitMediaScreen = () => {    
+    this.breakpointObserver.observe('(max-width: 640px)').subscribe((state: BreakpointState) => {      
+      if (state.matches) {
+        this.mediaMobile = true;
+      }else {
+        this.mediaMobile = false;
+      }
+    })
+  }  
+}
