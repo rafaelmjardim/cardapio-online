4e7bbbafadedf63c3887fcb4efeac7c029929b05
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-07-14 13:02:51 -0300

    update tabs dialog info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,14 @@ import { SubheaderComponent } from './components/subheader/subheader.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { LucideAngularModule, Menu, Home, BadgePercent, ShoppingBag, User, Search, XCircle, BaggageClaim } from "lucide-angular";
+import { LucideAngularModule, Menu, Home, BadgePercent, ShoppingBag, User, Search, XCircle, BaggageClaim, X } from "lucide-angular";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { CadastroProdutosModule } from './modules/cadastro-produtos/cadastro-produtos.module';
 import { LayoutModule } from "@angular/cdk/layout";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InfoDialogComponent } from './components/subheader/info-dialog/info-dialog.component';
+
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 @NgModule({
@@ -40,7 +42,8 @@ import { InfoDialogComponent } from './components/subheader/info-dialog/info-dia
     FormsModule,
     HomeModule,
     LayoutModule,
-    LucideAngularModule.pick({Menu, Home, BadgePercent, ShoppingBag, User, Search, XCircle, BaggageClaim}),
+    MatTabsModule,
+    LucideAngularModule.pick({Menu, Home, BadgePercent, ShoppingBag, User, Search, XCircle, BaggageClaim, X}),
     BrowserAnimationsModule
   ],
   exports: [
