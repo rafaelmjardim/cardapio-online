e6226abdd35046389ecc266d779b39715a6077f9
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-07-14 13:43:32 -0300

    filtros migrados para pagina home

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from './home';
 import { MobileUtilsService } from 'src/app/services/mobile_utils/mobile-utils.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
   products!: Product[];
 
   filter!: string;
 
-  constructor (public mobile_utils: MobileUtilsService){}
+  selectForm!: FormGroup;
+
+  constructor (public mobile_utils: MobileUtilsService, private form_builder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.selectForm = this.form_builder.group({
+      selectInput: ['']
+    })
+  }
   
+  onChangeCategoryValue = () => {
+    const optionValue = this.selectForm.controls['selectInput'].value;
+       
+    console.log(optionValue);
+  }
 }
