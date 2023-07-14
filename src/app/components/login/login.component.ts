7bd8df6b1b49cf29bc4feb5c52fee58208921969
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-07-14 17:00:56 -0300

    update dialog login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor (public dialog: MatDialog){}
 }
