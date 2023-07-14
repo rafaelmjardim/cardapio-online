ab6eb62803afc5c067cf9ed22e3eac17ea4b7961
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-07-14 18:38:53 -0300

    adicionado nome e numero ao localStorage

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -6,7 +7,26 @@ import { MatDialog } from '@angular/material/dialog';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor (public dialog: MatDialog){}
+  loginForm!: FormGroup;
+
+  constructor (public dialog: MatDialog, private form_builder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.loginForm = this.form_builder.group({
+      nameInput: [''],
+      phoneInput: ['']
+    })
+  }
+
+  submitLoginForm = () => {
+    const nameInput = this.loginForm.controls['nameInput'].value;
+    const phoneInput = this.loginForm.controls['phoneInput'].value;
+
+    localStorage.setItem('nome', nameInput);
+    localStorage.setItem('numero', phoneInput);
+
+    this.dialog.closeAll();
+  }
 }
