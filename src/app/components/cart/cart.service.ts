e631613467d7f3360a35e933bd87bdc4fb02676f
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-07-18 11:23:35 -0300

    update post pedido na api

--- src/app/components/cart/cart.service.ts
+++ src/app/components/cart/cart.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Product } from '../products-cards/products-cards';
 import { Pedido } from './cart';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environments.prod';
+
+const API_KEY = environment.API_KEY;
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +12,16 @@ import { HttpClient } from '@angular/common/http';
 export class CartService {
 
   cartItens: Product[] = []
-  // newCartItens: Cart[] = []
 
   constructor(private http: HttpClient) { }
 
   setCartItens = (product: Product) => {
     this.cartItens.push(product);
-    
-    // console.log(' items do servico',this.cartItens);
-    
+        
   }
 
   postPedido = (pedido: Pedido) => {
-    return this.http.post(`http://localhost:3000/pedido`, {
+    return this.http.post(`${API_KEY}/pedidos`, {
       id_cliente: pedido.id_cliente,
       mesa: pedido.mesa,
       lista_carrinho: pedido.lista_carrinho
