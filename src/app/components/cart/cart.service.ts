187a79d1c465645e60c02e023bb44523a9e0fb38
rafaelmjardim <rafaelmjardim@hotmail.com>
2023-07-19 22:36:24 -0300

    ajustado contagem de itens

--- src/app/components/cart/cart.service.ts
+++ src/app/components/cart/cart.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Product } from '../products-cards/products-cards';
 import { Order } from './cart';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environments.prod';
+import { Product } from '../products-cards/products-cards';
 
 const API_KEY = environment.API_KEY;
 
@@ -12,12 +12,15 @@ const API_KEY = environment.API_KEY;
 export class CartService {
 
   cartItens: Product[] = []
+  quantityCount: number = 0
 
   constructor(private http: HttpClient) { }
 
   setCartItens = (product: Product, quantity: number) => {
     this.cartItens.push({...product, quantidade: quantity});
-        
+
+    //Armazena uma contagem conforme é adicionado quantidade de item
+    this.quantityCount = this.quantityCount + quantity;            
   }
 
   postPedido = (pedido: Order) => {
