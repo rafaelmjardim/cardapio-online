592c607ed3760a9c483e99179c7e72d1a206fbe3
Rafael Jardim <rafaelmjardim@gmail.com>
2024-02-28 19:48:12 -0300

    update envirenment

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
 
-    API_KEY: 'http://177.137.118.192:9090'
+    API_KEY: 'http://177.137.118.186:9090'
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,5 +2,5 @@ export const environment = {
 
     production: true,
 
-    API_KEY: 'http://177.137.118.192:9090'
+    API_KEY: 'http://177.137.118.186:9090'
 };
